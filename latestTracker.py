08652a3b7c712717db0c7bab3095ef6d78fac299
josephkirankk <110154805+josephkirankk@users.noreply.github.com>
2023-09-22 21:39:51 +0530

    V 1.0 working

--- latestTracker.py
+++ latestTracker.py
@@ -6,23 +6,38 @@ import supervision as sv
 import numpy as np
 import cv2
 import time
+import threading
 
 
 HOME = os.getcwd()
 print(HOME)
 
 SOURCE_VIDEO_PATH = f"{HOME}/aku-v1.mp4"
+#MODEL = "astra_pop_nv1.pt"
 MODEL = "pop_n.pt"
 # settings
 # LINE_START = sv.Point(50, 1500)
 # LINE_END = sv.Point(3840-50, 1500)
 
+# lineArray = [(362, 35), (440, 478)]
+# LINE_START = lineArray[0]
+# LINE_END = lineArray[1]
+
 LINE_START = sv.Point(429, 178)
 LINE_END = sv.Point(539, 474)
 
 
+#uv-1
+#LINE_START = sv.Point(31, 114)
+#LINE_END = sv.Point(719, 213)
 
-TARGET_VIDEO_PATH = f"{HOME}/aku-v1-out.mp4"
+#uv-2
+# LINE_START = sv.Point(458, 192)
+# LINE_END = sv.Point(554, 477)
+
+
+
+TARGET_VIDEO_PATH = f"{HOME}/output.mp4"
 
 
 display.clear_output()
@@ -37,7 +52,7 @@ CLASS_NAMES_DICT = model.model.names
 
 # class_ids of interest - car, motorcycle, bus and truck
 #selected_classes = [2, 3, 5, 7]
-selected_classes = [0]
+selected_classes = [0,1]
 line_points = []
 
 def draw_line(event, x, y, flags, param):
@@ -82,6 +97,9 @@ def showOneFrame():
     anotated_frame=box_annotator.annotate(scene=frame, detections=detections, labels=labels)
     frame = anotated_frame
     #sv.plot_image(anotated_frame, (16,16))
+    
+    cv2.imshow('Frame', anotated_frame)
+    cv2.line(frame, LINE_START, LINE_END, (203, 255, 0), 2)
     cv2.imshow('Frame', anotated_frame)
     cv2.setMouseCallback('Frame', draw_line)
     cv2.waitKey(0)
@@ -90,13 +108,13 @@ def showOneFrame():
 
 sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
 # create BYTETracker instance
-byte_tracker = sv.ByteTrack(track_thresh= 0.25, track_buffer = 30,match_thresh = 0.8,frame_rate =20)
+byte_tracker = sv.ByteTrack(track_thresh= 0.25, track_buffer = 30,match_thresh = 0.8,frame_rate =30)
 
 # create VideoInfo instance
 video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
 
 # create frame generator
-generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH, stride=2)
+#generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH, stride=10)
 
 # create LineZone instance, it is previously called LineCounter class
 line_zone = sv.LineZone(start=LINE_START, end=LINE_END)
@@ -105,10 +123,14 @@ line_zone = sv.LineZone(start=LINE_START, end=LINE_END)
 box_annotator = sv.BoxAnnotator(thickness=4, text_thickness=4, text_scale=2)
 
 # create LineZoneAnnotator instance, it is previously called LineCounterAnnotator class
-line_zone_annotator = sv.LineZoneAnnotator(thickness=4, text_thickness=4, text_scale=2)
+line_zone_annotator = sv.LineZoneAnnotator(thickness=4, text_thickness=4, text_scale=1)
 
+frameCount = 0
 # define call back function to be used in video processing
 def callback(frame: np.ndarray, index:int) -> np.ndarray:
+    global frameCount
+    frameCount = frameCount + 1
+    print(frameCount)
     # model prediction on single frame and conversion to supervision Detections
     results = model(frame, verbose=False)[0]
     detections = sv.Detections.from_ultralytics(results)
@@ -131,19 +153,31 @@ def callback(frame: np.ndarray, index:int) -> np.ndarray:
     # return frame with box and line annotated result
     return  line_zone_annotator.annotate(box_annotated_frame, line_counter=line_zone)
 
-#showOneFrame()
 
-start_time = time.time()
+
 #process the whole video
-sv.process_video(
-    source_path = SOURCE_VIDEO_PATH,
-    target_path = TARGET_VIDEO_PATH,
-    callback=callback
-)
+# sv.process_video(
+#     source_path = SOURCE_VIDEO_PATH,
+#     target_path = TARGET_VIDEO_PATH,
+#     callback=callback
+# )
+def processVideo():
+    start_time = time.time()
+    source_video_info = sv.VideoInfo.from_video_path(video_path=SOURCE_VIDEO_PATH)
+    with sv.VideoSink(target_path=TARGET_VIDEO_PATH, video_info=source_video_info) as sink:
+        for index, frame in enumerate(
+            sv.get_video_frames_generator(source_path=SOURCE_VIDEO_PATH,stride=1)
+        ):
+            result_frame = callback(frame, index)
+            sink.write_frame(frame=result_frame)
 
-end_time = time.time()
+    end_time = time.time()
 
-# Calculate the duration
-duration = end_time - start_time
+    # Calculate the duration
+    duration = end_time - start_time
 
-print(f"Execution time: {duration:.6f} seconds")
+    print(f"Execution time: {duration:.6f} seconds")
+    
+
+#showOneFrame()
+processVideo()
